fefe68d4c56133a7ab42b4e9cd08a1714f4c63ee
Jean - Louis Macle <jeanlouis.macle@gmail.com>
2021-02-13 22:08:29 -0600

    Code update to reflect front-end code cleanup

--- src/test/java/jl/project/FirefoxTests/UserRequirement1.java
+++ src/test/java/jl/project/FirefoxTests/UserRequirement1.java
@@ -56,7 +56,7 @@ public class UserRequirement1 {
     	boolean isCategoryFound = false;
     	
     	
-    	driver.findElement(By.id("new_category_input_field")).sendKeys(testCategoryLabel);
+    	driver.findElement(By.id("new-category-input-field")).sendKeys(testCategoryLabel);
     	driver.findElement(By.id("add_category_button")).click();
     	//The category has been added. The display of the existing categories is being refreshed.
     	System.out.println("At this point, the test category should have been created.");
