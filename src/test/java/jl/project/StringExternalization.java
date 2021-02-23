268d1850d8b69a069b20df087d6d416b343f7b17
Jean-Louis <jeanlouis.macle@gmail.com>
2021-02-23 09:02:37 -0600

    Externalized the webdrivers folder

--- src/test/java/jl/project/StringExternalization.java
+++ src/test/java/jl/project/StringExternalization.java
@@ -3,7 +3,7 @@ package jl.project;
 public class StringExternalization {
 	public static final String front_end_url = "http://localhost:4200";
 	
-	public static final String webdrivers_folder = "webdrivers/";	
+	public static final String webdrivers_folder = "../webdrivers/";	
 	public static final String webdriver_chrome_key = "webdriver.chrome.driver";	
 	public static final String webdriver_firefox_key = "webdriver.gecko.driver";	
 	public static final String webdriver_edge_key = "webdriver.edge.driver";
