57f08f8fb587dfae8d8f9cfec6f246b09a97be9b
Jean-Louis <jeanlouis.macle@gmail.com>
2021-03-23 06:08:25 -0500

    Testing IBM Speech-to-test for potential use in screen reader testing automation

---
+++ src/test/java/jl/project/ScreenReadersTest/IBMSpeechToTextExample.java
@@ -0,0 +1,8 @@
+//https://www.ibm.com/cloud/watson-speech-to-text
+//https://cloud.ibm.com/docs/speech-to-text?topic=speech-to-text-gettingStarted#getting-started-tutorial
+
+package jl.project.ScreenReadersTest;
+
+public class IBMSpeechToTextExample {
+
+}
