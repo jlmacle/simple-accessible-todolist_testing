2cbda4e36191752a7301d009233288f20a245896
Jean-Louis <jeanlouis.macle@gmail.com>
2021-03-31 13:46:10 +0200

    Worked on the macOS version of the end-to-end script

--- end-to-end-tests-scripts/end-to-to-end-tests-scripts-macOS.sh
+++ end-to-end-tests-scripts/end-to-to-end-tests-scripts-macOS.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
-pwd
+echo "Starting the backend server" && pwd &&\
 cd ../../AccessibleTodoList_Backend && mvn spring-boot:run &> ../AccessibleTodoList_End2endTests/end-to-end-tests-scripts/log_SpringBoot-script.txt & #Re-directing both error and standard output to the file. 
 pwd
+echo "Starting the front-end server" &&\
 cd ../AccessibleTodoList_FrontEnd && ng serve -o &> ../AccessibleTodoList_End2endTests/end-to-end-tests-scripts/log_Angular-end-to-end-test-script.txt  & 
+echo "Starting the test suites"
 cd .. && sleep 120 && mvn test && kill$(lsof -nP -iTCP -sTCP:LISTEN | grep 4200 | sed 's/node//p' | sed 's/jl.*//p') && kill$(lsof -nP -iTCP -sTCP:LISTEN | grep 8080 | sed 's/java//p' | sed 's/jl.*//p')
 
 #lsof
