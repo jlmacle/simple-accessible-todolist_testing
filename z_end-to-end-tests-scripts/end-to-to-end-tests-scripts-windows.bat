d76dade6c6832f97ca22b8bbcda4d1a383b89c40
Jean-Louis <jeanlouis.macle@gmail.com>
2021-04-02 03:20:14 -0500

    Minor modification on commited files

--- z_end-to-end-tests-scripts/end-to-to-end-tests-scripts-windows.bat
+++ z_end-to-end-tests-scripts/end-to-to-end-tests-scripts-windows.bat
@@ -1,5 +1,5 @@
 rem To start from command console
 rem Numerous windows :)
-cmd.exe /c "echo "Starting the front-end server"  && cd ../src/test/java/jl/project/Pa11yTest/scripts && start run_Angular_server-windows.bat  && echo "Starting the front-end server"  && start run_Backend_server-windows.bat && echo "Starting the test suites"  && cd && cd ../../../../../../../ && cd && timeout /T 120 && mvn test &&  ./src/test/java/jl/project/Pa11yTest/scripts/stop_potentially_existing_Angular_Spring_server_processes-windows.bat"
+cmd.exe /c "echo "Starting the front-end server"  && cd ../src/test/java/jl/project/Pa11yTest/scripts && start run_Angular_server-windows.bat  && echo "Starting the front-end server"  && start run_Backend_server-windows.bat && echo "Starting the test suites"  && cd ../../../../../../../  && timeout /T 120 && mvn test &&  ./src/test/java/jl/project/Pa11yTest/scripts/stop_potentially_existing_Angular_Spring_server_processes-windows.bat"
 
 
