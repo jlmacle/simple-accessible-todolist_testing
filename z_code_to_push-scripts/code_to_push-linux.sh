eb6dbb7aca9c5a7e5fbf237c4baecfc242600e4b
jlmacle <jeanlouis.macle@gmail.com>
2021-04-06 16:37:17 +0200

    Added comments concerning the API key needed to test the Speech-to-text conversion.

--- z_code_to_push-scripts/code_to_push-linux.sh
+++ z_code_to_push-scripts/code_to_push-linux.sh
@@ -1,6 +1,8 @@
 #To add: localhost/no grid used by default
 
 #To add: code quality
+sudo docker start sonarqube
+
 
 echo "** mvn clean **"
 echo "The value of an apiKey stored in an environment variable has been dumped and later pushed on GitHub."
