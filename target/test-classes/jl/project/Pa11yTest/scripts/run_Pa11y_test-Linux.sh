bdae0e6fdd6a8ac1c1b5e63b52690e6f4767885e
Jean-Louis <jeanlouis.macle@gmail.com>
2021-04-07 10:00:41 -0500

    git add . after mvn clean

--- target/test-classes/jl/project/Pa11yTest/scripts/run_Pa11y_test-Linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR="../AccessibleTodoList_FrontEnd/node_modules/pa11y/bin"
-if [ -d "$DIR" ]; then
-	cd $DIR && ./pa11y.js http://localhost:4200
-else
-	pa11y http://localhost:4200 
-fi
