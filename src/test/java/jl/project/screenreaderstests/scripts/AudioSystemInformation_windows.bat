f8415c412dbf397101d9e945c508088e2cc40a13
Jean-Louis <jeanlouis.macle@gmail.com>
2021-04-12 10:28:47 -0500

    Code quality.

---
+++ src/test/java/jl/project/screenreaderstests/scripts/AudioSystemInformation_windows.bat
@@ -0,0 +1,4 @@
+echo "Starting the class AudioSystemInformation"
+java -cp C:/_Synchronized_Code/ATL/AccessibleTodoList_End2endTests/target/test-classes;C:/Users/jeanl/.m2/repository/org/slf4j/jcl-over-slf4j/1.7.30/jcl-over-slf4j-1.7.30.jar;C:/Users/jeanl/.m2/repository/org/slf4j/jul-to-slf4j/1.7.30/jul-to-slf4j-1.7.30.jar;C:/Users/jeanl/.m2/repository/org/slf4j/slf4j-api/1.7.13/slf4j-api-1.7.13.jar;C:/_Unsynchronized/slf4j-simple-1.7.30.jar   jl.project.screenreaderstests.AudioSystemInformation 
+
+:: https://repo1.maven.org/maven2/org/slf4j/slf4j-simple/1.7.30/
