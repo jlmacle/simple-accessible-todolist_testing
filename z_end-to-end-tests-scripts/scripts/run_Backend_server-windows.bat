906556639a3c5cd000b261fa2fad80354c1723e8
unknown <jeanlouis.macle@gmail.com>
2023-03-03 13:13:21 -0600

    Code cleanup

--- z_end-to-end-tests-scripts/scripts/run_Backend_server-windows.bat
+++
@@ -1,4 +0,0 @@
-taskkill -T /F /FI "WINDOWTITLE eq spring_server_parent*"
-echo "Waiting for a potential SpringBoot server process to be suppressed."
-timeout /T 5 
-start "spring_server_parent" run_Backend_server-windows_forBackendServerStartup.bat
