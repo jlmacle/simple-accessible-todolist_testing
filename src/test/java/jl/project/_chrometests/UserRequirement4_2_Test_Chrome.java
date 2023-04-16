af3a7c24c0d34344a207f4e222870337befce96d
Jean-Louis Macle <jeanlouis.macle@gmail.com>
2023-04-16 08:24:32 -0500

    Code cleanup

--- src/test/java/jl/project/_chrometests/UserRequirement4_2_Test_Chrome.java
+++ src/test/java/jl/project/_chrometests/UserRequirement4_2_Test_Chrome.java
@@ -1,4 +1,4 @@
-package jl.project._edgetests;
+package jl.project._chrometests;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,7 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-
 import org.testng.annotations.Test;
 
 import jl.project.StringExternalization;
@@ -20,41 +19,43 @@ import jl.project.__commontests.RobotFactory;
 import jl.project.__commontests.tests.TestsUtilCommon;
 import jl.project.__commontests.tests.TestsUtilWithKeyboard;
 
-
 /**
  * @author
  * Class testing the user requirement of physical impairment web accessibility 
  * using the keyboard only - Space key used.
  */
-public class UserRequirement4_2_Test {
+public class UserRequirement4_2_Test_Chrome {
+	Logger logger = LoggerFactory.getLogger(jl.project._chrometests.UserRequirement4_2_Test_Chrome.class);
 	/* Note: delaying or not the sending of the keys impact the success of the tests */
-	Logger logger = LoggerFactory.getLogger(jl.project._edgetests.UserRequirement4_2_Test.class);
-	WebDriver driver;
+	WebDriver driver;	
 	Robot robot = RobotFactory.getRobotInstance();
-		
+	
 	@BeforeClass
 	public void setup() 
 	{
 		robot = RobotFactory.getRobotInstance();
 		
-		driver = TestsUtilCommon.setup(logger,robot, StringExternalization.BROWSER_NAME_EDGE, driver, StringExternalization.WEBDRIVER_KEY_EDGE,StringExternalization.WEBDRIVER_VALUE_EDGE);		
+		driver = TestsUtilCommon.setup(logger,robot, StringExternalization.BROWSER_NAME_CHROME, driver, StringExternalization.WEBDRIVER_KEY_CHROME,StringExternalization.WEBDRIVER_VALUE_CHROME);
+	
 	}
 	
 	@BeforeMethod
-	public void navigate() {
+	public void navigate() 
+	{
 		driver.get(StringExternalization.ANGULAR_SERVER_URL);
 		
 	}
-	
-	@Test(groups = {"creation_deletion_edge_2"})	
+
+	@Test(groups = {"creation_deletion_Chrome_2"})		
 	public void createAndDeleteACategoryWithKeyboardOnly_SpaceKey() 
 	{
 		boolean isTestSuccessful = false;
 		isTestSuccessful = TestsUtilWithKeyboard.createAndDeleteCategory_UsingTheKeyboard(logger, driver, robot, Keys.SPACE, StringExternalization.TEST_KEYBOARD_SPACE_KEY);
-		assertThat(isTestSuccessful).isTrue();	
+		assertThat(isTestSuccessful).isTrue();			
 	}
+		
+	@Test(groups = {"creation_deletion_Chrome_2"})	
 	
-	@Test(groups = {"creation_deletion_edge_2"})					
 	public void createAndDeleteItemWithKeyboardOnly_SpaceKey() 
 	{
 		boolean isTestSuccessful = false;
@@ -62,12 +63,13 @@ public class UserRequirement4_2_Test {
 		assertThat(isTestSuccessful).isTrue();	
 	}
 	
-	@Test(dependsOnGroups = {"creation_deletion_edge_2"})		
-	public void HideAndDisplayItemsWithKeyboardOnly_SpaceKey() 
+	@Test(dependsOnGroups = {"creation_deletion_Chrome_2"})		
+	public void hideAndDisplayItemsWithKeyboardOnly_SpaceKey() 
 	{
 		boolean isTestSuccessful = false;
 		isTestSuccessful = TestsUtilWithKeyboard.hideAndDisplayItems_UsingTheKeyboard(logger, driver, robot, Keys.SPACE, StringExternalization.TEST_KEYBOARD_SPACE_KEY);
 		assertThat(isTestSuccessful).isTrue();	
+			
 	}
 	
 	
